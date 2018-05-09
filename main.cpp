8dc47c2f46bdf7780cd021a98a1c2981c55e9000
Mario <mario2364@yandex.ru>
2018-05-09 17:58:46 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "sdt.h"
+#include <locale.h>
+
+using namespace std;
+ 
+int main()
+{
+	int a; int b;
+cout << "Введите первое число";
+cin >> a;
+cout << "Введите второе число";
+cin >> b;
+
+	return 0;
+}
